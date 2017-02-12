41f74c7e6ef175b7447b1e82ad98e9f611a41c3b
Winson Wong <winson0228@gmail.com>
2017-02-12 15:20:44 -0800

    Adding main menu

---
+++ bens-shadow/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+	private LevelManager gc;
+
+
+	public void LoadLevel(string name){
+		Debug.Log ("New Level load: " + name);
+		Application.LoadLevel (name);
+	}
+
+	public void QuitRequest(){
+		Debug.Log ("Quit requested");
+		Application.Quit ();
+	}
+
+	void Update() {
+		if (Input.GetKey ("joystick button 0")) {
+			Application.LoadLevel ("Level 1");
+		}
+	}
+
+}
