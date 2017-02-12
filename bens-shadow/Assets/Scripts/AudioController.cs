72a3b93d3a0a41d8f0a4b57b727ffc94a2a5a2be
michaelenglo <michaelenglo10@yahoo.co.id>
2017-02-12 12:52:48 -0800

    add step sound

--- bens-shadow/Assets/Scripts/AudioController.cs
+++ bens-shadow/Assets/Scripts/AudioController.cs
@@ -7,13 +7,31 @@ public class AudioController : MonoBehaviour {
 	private GameController gc;
 
 	public AudioSource audioSource;
+	public AudioSource stepSoundSource;
 
  	public AudioClip RealDimensionMusic;
  	public AudioClip ShadowDimensionMusic;
+	public AudioClip StepSoundClip;
+
+	public bool CanPlayStep = true;
 
 	void Start () {
 		gc = GameObject.FindWithTag("GameController").GetComponent<GameController>();
+		stepSoundSource.clip = StepSoundClip;
 		changeMusic();
+	}
+
+	void FixedUpdate () {
+		if (CanPlayStep && !stepSoundSource.isPlaying && (Input.GetAxis ("Horizontal") != 0 || Input.GetAxis ("Vertical") != 0)) {
+			stepSoundSource.Play ();
+			CanPlayStep = false;
+			StartCoroutine(PlayDelayedSound(0.3f - 0.1f * (new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical")).magnitude)));
+		}
+	}
+
+	IEnumerator PlayDelayedSound(float delay){
+		yield return new WaitForSecondsRealtime (delay);
+		CanPlayStep = true;
 	}
 
 	public void changeMusic () {
