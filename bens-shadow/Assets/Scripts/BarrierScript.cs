6e377a40ccaa7d17728eef7e7e7df4916164617a
Winson Wong <winson0228@gmail.com>
2017-02-11 17:24:08 -0800

    barrier toggle script 1

---
+++ bens-shadow/Assets/Scripts/BarrierScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrierScript : MonoBehaviour, IActivatable {
+	private bool isOff;
+	// Use this for initialization
+	void Start () {
+		isOff = false;
+	}
+
+
+	public void Activate() {
+		isOff = !isOff;
+		Debug.Log (isOff);
+		this.GetComponent<BoxCollider2D> ().enabled = !isOff;
+	}
+}
