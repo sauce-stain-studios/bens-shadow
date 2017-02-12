e96229124242c05b50a22703d21a3e5d776a5ec8
Winson Wong <winson0228@gmail.com>
2017-02-11 22:19:05 -0800

    Added hint revealing script, attach scipts to hint game objects according to dimension

---
+++ bens-shadow/Assets/Scripts/NumHintShadBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumHintShadBehaviour : MonoBehaviour {
+
+	private GameController gc;
+	// Use this for initialization
+	void Start () {
+		gc = GameObject.FindWithTag("GameController").GetComponent<GameController>();
+
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		if (gc.getCurrentDimension() == GameController.Dimension.Shadow) {
+			gameObject.GetComponent<Renderer>().enabled = true;
+		} else {
+			gameObject.GetComponent<Renderer>().enabled = false;
+
+		}
+	}
+}
