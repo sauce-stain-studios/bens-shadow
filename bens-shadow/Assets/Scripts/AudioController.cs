ef61887d8baf9cc36a5a2c668acb7b3076050f0b
Rafael Jacinto <rpdjacinto@gmail.com>
2017-02-12 11:00:43 -0800

    Fixed AudioController

--- bens-shadow/Assets/Scripts/AudioController.cs
+++ bens-shadow/Assets/Scripts/AudioController.cs
@@ -15,29 +15,26 @@ public class AudioController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		gc = GameObject.FindWithTag("GameController").GetComponent<GameController>();
-		pausedTime = 0;
+		// pausedTime = 0;
 		changeMusic();
 	}
 
 	public void changeMusic () {
+		float oldTime = audio.time;
 		if (gc.getCurrentDimension() == GameController.Dimension.Real) {
 			audio.clip = RealDimensionMusic;
-			audio.time = pausedTime;
-			audio.Play();
 		} else {
 			audio.clip = ShadowDimensionMusic;
-			audio.time = pausedTime;
-			audio.Play();
 		}
+		audio.Play();
 	}
 
 	void Update() {
-		if (pausedTime <= 170) {
-				pausedTime += Time.deltaTime;
-			} else {
-				pausedTime = 0;
-			}
-		float timec = 0;
-		Debug.Log (audio.time);
+		// if (pausedTime <= 170) {
+		// 	pausedTime += Time.deltaTime;
+		// } else {
+		// 	pausedTime = 0;
+		// }
+		// Debug.Log (audio.time);
 	}
 }
