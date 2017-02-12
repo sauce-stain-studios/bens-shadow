6fbb640a581de99ca122da115fff24bc4f1f4eb6
winnie151 <jingwenchenw@gmail.com>
2017-02-11 18:50:24 -0800

    Added Directional Light

---
+++ bens-shadow/Assets/Scripts/ChangeLightIntensity.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeLightIntensity : MonoBehaviour {
+
+    public GameController gc;
+    public float bright = 1.0F;
+    public float dark = 0.5F;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        gc = GameObject.FindWithTag("GameController").GetComponent<GameController>();
+        GetComponent<Light>().intensity = dark;
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (gc.getCurrentDimension() == GameController.Dimension.Real)
+        {
+            GetComponent<Light>().intensity = dark;
+        }
+        else
+        {
+            GetComponent<Light>().intensity = bright;
+        }
+    }
+}
