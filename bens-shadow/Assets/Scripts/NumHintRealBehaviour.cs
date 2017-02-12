c49b53b4196fbb6f6e3cd54371508f4b2cf3f37f
Winson Wong <winson0228@gmail.com>
2017-02-11 22:19:05 -0800

    Added hint revealing script, attach scipts to hint game objects according to dimension

---
+++ bens-shadow/Assets/Scripts/NumHintRealBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumHintRealBehaviour : MonoBehaviour {
+
+	private GameController gc;
+	private GameObject hint;
+	private bool destroyed;
+	// Use this for initialization
+	void Start () {
+		hint = gameObject;
+		destroyed = true;
+		gc = GameObject.FindWithTag("GameController").GetComponent<GameController>();
+
+	}
+		
+
+	// Update is called once per frame
+	void Update () {
+		if (gc.getCurrentDimension() == GameController.Dimension.Real) {
+			gameObject.GetComponent<Renderer>().enabled = true;
+		} else {
+			gameObject.GetComponent<Renderer>().enabled = false;
+		}
+}
+}
