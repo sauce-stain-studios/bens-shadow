9215f38f5d595c787642f30a0196bd3ee9b260da
Winson Wong <winson0228@gmail.com>
2017-02-12 10:11:08 -0800

    continue music after switch

--- bens-shadow/Assets/Scripts/AudioController.cs
+++ bens-shadow/Assets/Scripts/AudioController.cs
@@ -6,24 +6,38 @@ public class AudioController : MonoBehaviour {
 
 	private GameController gc;
 
-	public AudioSource audioSource;
+	public AudioSource audio;
 
  	public AudioClip RealDimensionMusic;
  	public AudioClip ShadowDimensionMusic;
+	private float pausedTime;
 
 	// Use this for initialization
 	void Start () {
 		gc = GameObject.FindWithTag("GameController").GetComponent<GameController>();
+		pausedTime = 0;
 		changeMusic();
 	}
 
 	public void changeMusic () {
 		if (gc.getCurrentDimension() == GameController.Dimension.Real) {
-				audioSource.clip = RealDimensionMusic;
-				audioSource.Play();
+			audio.clip = RealDimensionMusic;
+			audio.time = pausedTime;
+			audio.Play();
 		} else {
-				audioSource.clip = ShadowDimensionMusic;
-				audioSource.Play();
+			audio.clip = ShadowDimensionMusic;
+			audio.time = pausedTime;
+			audio.Play();
 		}
+	}
+
+	void Update() {
+		if (pausedTime <= 170) {
+				pausedTime += Time.deltaTime;
+			} else {
+				pausedTime = 0;
+			}
+		float timec = 0;
+		Debug.Log (audio.time);
 	}
 }
