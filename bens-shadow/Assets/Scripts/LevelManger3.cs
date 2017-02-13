b9064ee9d36a24210c70783ebc4b97bf3c2bc12f
winnie151 <jingwenchenw@gmail.com>
2017-02-12 16:13:57 -0800

    Added Story Scenes

---
+++ bens-shadow/Assets/Scripts/LevelManger3.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManger3 : MonoBehaviour
+{
+    private LevelManager gc;
+
+
+    public void LoadLevel(string name)
+    {
+        Debug.Log("New Level load: " + name);
+        Application.LoadLevel(name);
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("Quit requested");
+        Application.Quit();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey("joystick button 0"))
+        {
+            Application.LoadLevel("Level 1");
+        }
+    }
+
+}
